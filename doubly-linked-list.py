59d2aa4a608692ea9a2300d7f7e5987619fc4cec
Ritvik Vasantha Kumar <rv2459@nyu.edu>
2025-04-19 12:52:22 -0400

    Add missing Node class definition

--- doubly-linked-list.py
+++ doubly-linked-list.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
