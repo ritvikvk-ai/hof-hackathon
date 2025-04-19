784b2fd1e273d5aa22e3787a37003908e54582b3
Ritvik Vasantha Kumar <rv2459@nyu.edu>
2025-04-19 13:29:42 -0400

    Update and rename class-doubly.py to node_dll.py

--- class-doubly.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
---
+++ node_dll.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
+        new_node.prev = curr
